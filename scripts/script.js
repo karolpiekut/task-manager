7c96eb7c1dc335c314db83734a7dbc32d023c7df
kpiekut <karol.piekut@pm.me>
2024-01-27 11:14:12 +0000

    add basic button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
-console.log('test');
+const button = document.querySelector('#add-task');
+const tasksDiv = document.querySelector('#tasks');
+let number = 1;
+
+let taskList = [];
+
+
+function addTask() {
+    const para = document.createElement('p');
+    const node = document.createTextNode(`This is number ${number}`);
+    taskList.push(`This is number ${number}`);
+    para.appendChild(node);
+    tasksDiv.appendChild(para);
+    number++;
+    console.table(taskList);
+}
+
+button.addEventListener('click', addTask);
