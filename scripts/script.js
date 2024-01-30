3fa19d240ec010730686ce84a9d9f22dadbe4f0d
kpiekut <karol.piekut@pm.me>
2024-01-30 12:36:12 +0000

    add create a basic task function

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,57 @@
-
-const button = document.querySelector('#add-task');
-const tasksDiv = document.querySelector('#tasks');
+const button = document.querySelector("#add-task");
+const tasksDiv = document.querySelector("#tasks");
 let number = 1;
 
 let taskList = [];
 
 const statusList = {
-    not_started: 0,
-    in_progress: 1,
-    complete: 2,
-    cancelled: 3,
-    expired: 4
-}
+  not_started: 0,
+  in_progress: 1,
+  complete: 2,
+  cancelled: 3,
+  expired: 4,
+};
 
 class TaskTemplate {
-    constructor(title, text, date, status) {
-        this.title = title;
-        this.text = text;
-        this.date = date;
-        this.status = status;
-    }
+  constructor(title, text, date, status) {
+    this.title = title;
+    this.text = text;
+    this.date = date;
+    this.status = status;
+  }
 }
 
-const completeTutorial = new TaskTemplate("complete tutorial", "work on the tutorial", "2024-30-01", "not-started");
+const completeTutorial = new TaskTemplate(
+  "complete tutorial",
+  "work on the tutorial",
+  "2024-30-01",
+  "not-started"
+);
 
 console.log(completeTutorial);
-
 
 console.log(statusList.in_progress);
 
 
-
-
-
-
 function addTask() {
-    const para = document.createElement('p');
-    const node = document.createTextNode(`This is number ${number}`);
-    taskList.push(`This is number ${number}`);
-    para.appendChild(node);
-    tasksDiv.appendChild(para);
-    number++;
-    console.table(taskList);
+    let addTaskOne = prompt("Please enter task name:", "new");
+    let addTaskText = prompt("Please enter task text:", "task text");
+    let addTaskDate = prompt("Please enter task due date:", "1900-01-01");
+    let addTaskStatus = prompt("Please enter task status", "new");
+    
+    let tempTask = new TaskTemplate(addTaskOne, addTaskText, addTaskDate, addTaskStatus
+    );
+
+    taskList.push(tempTask);
+    
+    //const para = document.createElement('p');
+    //const node = document.createTextNode(`This is number ${number}`);
+    //taskList.push(`This is number ${number}`);
+    //para.appendChild(node);
+    //tasksDiv.appendChild(para);
+    //number++;
+    //console.table(taskList);
 }
 
 button.addEventListener('click', addTask);
+
