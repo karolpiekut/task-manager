59326860679da2802ab23fa21278673f5589d456
kpiekut <karol.piekut@pm.me>
2024-02-27 06:44:35 +0000

    recreate prototype

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
-            },
+            }
         ],
         },
 
