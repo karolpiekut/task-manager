7f012dc4b1cfbb1b2255d564cae4c51db508d125
kpiekut <karol.piekut@pm.me>
2024-03-26 07:24:23 +0000

    fix body.js

--- src/body.js
+++ src/body.js
@@ -1,34 +1,124 @@
-function component() {
-    const element = document.createElement('div');
-    //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.innerHTML = `<div id="projects">
-    <div id="projects-list">
-        <h2>projects</h2>
-        <div id="each-project"></div>
-    </div>
-    <div id="plan" class="work-pane">
-        <h2>plan</h2>
-        <div class="board" id="plan-board"></div>
-    </div>
-    <div id="today" class="work-pane">
-        <h2>today</h2>
-        <div class="board"></div>
-    </div>
-    <div id="future" class="work-pane">
-        <h2>future</h2>
-        <div class="board"></div>
-    </div>
-    <div id="overdue" class="work-pane">
-        <h2>overdue</h2>
-        <div class="board"></div>
-    </div>
-    <div id="done" class="work-pane">
-        <h2>done</h2>
-        <div class="board"></div>
-    </div>
-</div>
-<button id="add-task">add task</button>`
-    return element;
+function appBody() {
+    const theApp = document.createElement('div');
+    theApp.setAttribute('id','theApp');
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.setAttribute('id', 'buttonsDiv');
+    theApp.appendChild(buttonsDiv);
+
+    const addProjectButton = document.createElement('button');
+    addProjectButton.setAttribute('id', 'add-project');
+    addProjectButton.innerText = 'add project';
+    buttonsDiv.appendChild(addProjectButton);
+
+    const addTaskButton = document.createElement('button');
+    addTaskButton.setAttribute('id', 'add-task');
+    addTaskButton.innerText = 'add task';
+    buttonsDiv.appendChild(addTaskButton);
+
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+
+    const projectsDiv = document.createElement('div');
+    projectsDiv.setAttribute('id', 'projects');
+    theApp.appendChild(projectsDiv);
+
+    const projectsHeader = document.createElement('h2');
+    projectsHeader.innerText = "projects";
+    projectsDiv.appendChild(projectsHeader);
+
+    const projectsList  = document.createElement('div');
+    projectsList.setAttribute('id','project-list');
+    projectsDiv.appendChild(projectsList);
+
+    //--------------------------------------------------------------------------
+    //--------------------------------------------------------------------------
+    const tasksDiv = document.createElement('div');
+    tasksDiv.setAttribute('id', 'all-tasks-board');
+    theApp.appendChild(tasksDiv);
+
+    //--------------------------------------------------------------------------
+    const planColumn = document.createElement('div')
+    planColumn.setAttribute('id', 'plan');
+    planColumn.setAttribute('class', 'work-pane');
+    tasksDiv.appendChild(planColumn);
+
+    const planHeader = document.createElement('h2');
+    planHeader.innerText = 'plan';
+    planColumn.appendChild(planHeader);
+
+    const planBoard = document.createElement('div');
+    planBoard.setAttribute('class', 'board');
+    planBoard.setAttribute('id', 'plan-board');
+    planColumn.appendChild(planBoard);
+
+    //--------------------------------------------------------------------------
+    const todayColumn = document.createElement('div')
+    todayColumn.setAttribute('id', 'today');
+    todayColumn.setAttribute('class', 'work-pane');
+    tasksDiv.appendChild(todayColumn);
+
+    const todayHeader = document.createElement('h2');
+    todayHeader.innerText = 'today';
+    todayColumn.appendChild(todayHeader);
+
+    const todayBoard = document.createElement('div');
+    todayBoard.setAttribute('class', 'board');
+    todayBoard.setAttribute('id', 'today-board' );
+    todayColumn.appendChild(todayBoard);
+
+    //--------------------------------------------------------------------------
+    const futureColumn = document.createElement('div')
+    futureColumn.setAttribute('id', 'future');
+    futureColumn.setAttribute('class', 'work-pane');
+    tasksDiv.appendChild(futureColumn);
+
+    const futureHeader = document.createElement('h2');
+    futureHeader.innerText = 'future';
+    futureColumn.appendChild(futureHeader);
+    
+    const futureBoard = document.createElement('div');
+    futureBoard.setAttribute('class', 'board');
+    futureBoard.setAttribute('id', 'future-board');
+    futureColumn.appendChild(futureBoard)
+
+    //--------------------------------------------------------------------------
+    const overdueColumn = document.createElement('div')
+    overdueColumn.setAttribute('id', 'overdue');
+    overdueColumn.setAttribute('class', 'work-pane');
+    tasksDiv.appendChild(overdueColumn);
+
+    const overdueHeader = document.createElement('h2');
+    overdueHeader.innerText = 'overdue';
+    overdueColumn.appendChild(overdueHeader);
+
+    const overdueBoard = document.createElement('div');
+    overdueBoard.setAttribute('class', 'board')
+    overdueBoard.setAttribute('id', 'overdue-board');
+    overdueColumn.appendChild(overdueBoard);
+
+    //--------------------------------------------------------------------------
+    const doneColumn = document.createElement('div')
+    doneColumn.setAttribute('id', 'done');
+    doneColumn.setAttribute('class', 'work-pane');
+    tasksDiv.appendChild(doneColumn);
+
+    const doneHeader = document.createElement('h2');
+    doneHeader.innerText = 'done';
+    doneColumn.appendChild(doneHeader);
+
+    const doneBoard = document.createElement('div');
+    doneBoard.setAttribute('class', 'board');
+    doneBoard.setAttribute('id', 'done-board');
+    doneColumn.appendChild(doneBoard);
+
+    //--------------------------------------------------------------------------
+
+
+
+    return theApp;
 }
 
-export default component();
+export default appBody();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles.css';
 import addTask from "./tasks.js";
-import component from "./body.js";
+import appBody from "./body.js";
 
-document.body.appendChild(component);
+document.body.appendChild(appBody);
 const button = document.querySelector("#add-task");
 button.addEventListener("click", addTask);
