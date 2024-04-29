c1786088db504f38b5323533049f6874dc32fc7c
kpiekut <karol.piekut@pm.me>
2024-04-29 13:26:11 +0100

    remove redundant lines

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const theApp = document.querySelector("#theApp");
 let tasks = [];
 let customTaskIndex = -1;
 
-
 function createATaskDiv(projectName, taskName, text, date, status) {
     const taskDiv = document.createElement('div');
     taskDiv.setAttribute('id', 'taskDiv');
@@ -38,7 +37,6 @@ function createATaskDiv(projectName, taskName, text, date, status) {
     return taskDiv;
 }
 
-
 //create tasks
 function Task(projectName, taskName, text, date, status) {
     customTaskIndex++;
@@ -53,16 +51,14 @@ function Task(projectName, taskName, text, date, status) {
     }
 }
 
-
 // function createATask (projectName, taskName, text, date, status){
-    function createATask (){
+function createATask() {
     const plan = document.querySelector("#plan-board");
-    let projectNameQuestion= prompt("Project name:", "new");
+    let projectNameQuestion = prompt("Project name:", "new");
     let taskNameQuestion = prompt("Task name:", "new")
     let taskTextQuestion = prompt("Notes:", "new");
     let taskDateQuestion = prompt("Task due:", "1900-01-01");
     let taskStatusQuestion = prompt("Please enter task status", "new");
-
 
     let tempTask = Task(projectNameQuestion,
         taskNameQuestion,
@@ -70,20 +66,17 @@ function Task(projectName, taskName, text, date, status) {
         taskDateQuestion,
         taskStatusQuestion)
 
-        console.log(tempTask);
     tasks.push(tempTask);
 
-        plan.appendChild(createATaskDiv(
-            projectNameQuestion,
-            taskNameQuestion,
-            taskTextQuestion,
-            taskDateQuestion,
-            taskStatusQuestion));
+    plan.appendChild(createATaskDiv(
+        projectNameQuestion,
+        taskNameQuestion,
+        taskTextQuestion,
+        taskDateQuestion,
+        taskStatusQuestion));
 
-    // plan.appendChild(createATaskDiv(projectName, taskName, text, date, status));
-    // tasks.push(Task(projectName,taskName, text, date, status));
+    console.table(tasks);
 }
-
 
 //delete tasks
 function removeTask(array, taskId) {
@@ -110,126 +103,4 @@ function amendValues(array, id, property, value) {
     }
 }
 
-
-// function addTask() {
-//     const tasksDiv = document.querySelector("#plan-board");
-//     let addTaskOne = prompt("Please enter task name:", "new");
-//     let addTaskText = prompt("Please enter task text:", "task text");
-//     let addTaskDate = prompt("Please enter task due date:", "1900-01-01");
-//     let addtaskstatus = prompt("Please enter task status", "new");
-//     let tempTask = new TaskTemplate(
-//         addTaskOne,
-//         addTaskText,
-//         addTaskDate,
-//         addtaskstatus
-//     );
-//     taskList.push(tempTask);
-//     const para = document.createElement('div');
-//     const node = document.createTextNode(`This is number ${number}`);
-//     taskList.push(`This is number ${number}`);
-//     para.appendChild(node);
-//     para.setAttribute('class', 'task');
-//     tasksDiv.appendChild(para);
-//     number++;
-// }
-//
-
-
-
-// let app = [];
-// let taskList = [];
-// let number = 0;
-// let projectNumber = 0;
-
-//
-// const statusList = {
-//     notStarted: 0,
-//     inProgress: 1,
-//     complete: 2,
-//     cancelled: 3,
-//     expired: 4,
-// };
-//
-// //Object.setPrototypeOf(Player.prototype, Person.prototype)
-//
-// class ProjectTemplate {
-//     constructor(title) {
-//         this.title = title;
-//     }
-// }
-//
-// class TaskTemplate extends ProjectTemplate{
-//     constructor(taskTitle, text, date, status) {
-//         this.taskTitle = taskTitle;
-//         this.text = text;
-//         this.date = date;
-//         this.status = status;
-//     }
-// }
-//
-// function addProject() {
-//     const projectsDiv = document.querySelector('#project-list');
-//     let addProjectName = prompt("Please enter project name:", "new");
-//     let tempProject = new ProjectTemplate(
-//         addProjectName
-//     );
-//     app.push(tempProject);
-//
-//     const paraProject = document.createElement('div');
-//     const nodeProject = document.createTextNode(`${addProjectName}`)
-//     paraProject.appendChild(nodeProject)
-//     projectsDiv.appendChild(paraProject);
-//     projectNumber++;
-// }
-//
-
-// function addTask() {
-//     const tasksDiv = document.querySelector("#plan-board");
-//     let addTaskOne = prompt("Please enter task name:", "new");
-//     let addTaskText = prompt("Please enter task text:", "task text");
-//     let addTaskDate = prompt("Please enter task due date:", "1900-01-01");
-//     let addtaskstatus = prompt("Please enter task status", "new");
-//     let tempTask = new TaskTemplate(
-//         addTaskOne,
-//         addTaskText,
-//         addTaskDate,
-//         addtaskstatus
-//     );
-//     taskList.push(tempTask);
-//     const para = document.createElement('div');
-//     const node = document.createTextNode(`This is number ${number}`);
-//     taskList.push(`This is number ${number}`);
-//     para.appendChild(node);
-//     para.setAttribute('class', 'task');
-//     tasksDiv.appendChild(para);
-//     number++;
-// }
-
-// tasks.push(Task("task-manager", "name", "random text", "2024-04-10", 'new'));
-// tasks.push(Task("task-manager", "name2", "random text2", "2024-04-11", 'done'));
-// tasks.push(Task("food menu", "project scope", "create a project scope", "2024-04-10", 'overdue'));
-// tasks.push(Task("food menu", "create recipes", "get food recipes from granny", "2024-04-20", 'new'));
-// // tasks.push(Task("odin-project", "name3", "test tekstu", "2024-04-14", 'overdue'));
-// // tasks.push(Task("odin-project", "name4", "test tekstu", "2024-04-14", 'overdue'));
-// // tasks.push(Task("task-manager", "name3", "test tekstu", "2024-04-14", 'overdue'));
-// // tasks.push(Task("task-manager", "name5", "random tekst", "2024-04-19", 'done'));
-// // tasks.push(Task("odin-project", "name8", "ogarnij", "2024-04-14", 'overdue'));
-
-
-// export {addTask, addProject};
-// =======
-
-//
-//
-//
-
 export {createATaskDiv, Task, removeTask, removeProject, amendValues, createATask};
-
-
-
-
-//amendValues(tasks, 1, "projectName", "task manager");
-//amendValues(tasks, 1, "taskName", "Amend Tasks Function");
-
-//console.table(tasks);
-// >>>>>>> frontend
--- src/index.js
+++ src/index.js
@@ -9,14 +9,3 @@ const projectButton = document.querySelector('#add-project');
 taskButton.addEventListener('click', createATask);
 //projectButton.addEventListener('click', addProject);
 
-
-
-// createATask("task-manager", "name", "random text", "2024-04-10", 'new');
-// createATask("task-manager", "name2", "random text2", "2024-04-11", 'done');
-// createATask("food menu", "project scope", "create a project scope", "2024-04-10", 'overdue');
-// createATask("food menu", "create recipes", "get food recipes from granny", "2024-04-20", 'new');
-// createATask("odin-project", "name3", "test tekstu", "2024-04-14", 'overdue');
-// createATask("odin-project", "name4", "test tekstu", "2024-04-14", 'overdue');
-// createATask("task-manager", "name3", "test tekstu", "2024-04-14", 'overdue');
-// createATask("task-manager", "name5", "random tekst", "2024-04-19", 'done');
-// createATask("odin-project", "name8", "ogarnij", "2024-04-14", 'overdue');
